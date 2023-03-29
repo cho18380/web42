30eceb54709b31d1397b530d32d4eec353bb40ed
chosunjae <cho18380@gmail.com>
2023-03-29 14:21:35 +0900

    2323

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=200)
     content = models.TextField()
+    image_link = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
