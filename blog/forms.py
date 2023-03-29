51793378a41285da1aead195b04db8b2c63d6c89
chosunjae <cho18380@gmail.com>
2023-03-29 20:27:03 +0900

    2323

--- blog/forms.py
+++ blog/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
-    image_link = forms.CharField(max_length=1255, required=False) # 이미지 링크 입력 필드
 
     class Meta:
         model = Post
-        fields = ['title', 'content', 'image_link']
+        fields = ['title', 'content', 'image_link', 'overview']
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 
 class Author(models.Model):
@@ -10,10 +11,16 @@ class Author(models.Model):
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = RichTextField()
     image_link = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    overview = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.overview:
+            self.overview = self.content[:200] + '...' # set overview to first 200 characters of content plus ellipsis
+        super().save(*args, **kwargs)
